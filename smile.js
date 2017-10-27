c71fd6ebee64d450ae10d99bcf565148585f6b76
Huss <hussein@joincoded.com>
2017-10-27 12:42:18 +0300

    timer added

--- smile.js
+++ smile.js
@@ -2,9 +2,16 @@ var attempts = 0;
 var smileTable = document.querySelector('#smiletable'); // select the table from smile.html
 var winOrLose = document.querySelector('#winlose');
 var attemptCountDisplay = document.querySelector('#attemptcount');	//number of guessing attempts
-attemptCountDisplay.innerHTML = "You have <span style='color:green;'>4</span> chances to find the happy face ;)";
+attemptCountDisplay.innerHTML = "You have <span style='color:green;'>4</span> chances and <span style='color:red;'>10 seconds</span> to find the happy face ;)";
 var randomTextArray = ['Try harder.', 'You can do this!', 'Don\'t stop now!']; // some encouraging messages ;D
-var gameOver = false; 								// gameOver boolean to check the status of the game. game over if true.
+var gameOver = false;								// gameOver boolean to check the status of the game. game over if true
+
+	var stopTime = setTimeout(function terminate() {
+		alert("TIME'S UP!");
+		gameOver=true;
+		attemptCountDisplay.innerHTML='You ran out of time :( Click on any face to try again'}, 10000);
+
+
 /*TABLE CREATION */
 var rowNum = 1; 									// the first row will carry this value
 for(i=1; i<=4; i++){ 								// row creation loop
@@ -51,13 +58,14 @@ function winLose(img, imgId){
 	console.log("random: " + randomNum);
 	*/
 
-	if(randomNum === imgId && img.class != 'sad' && gameOver==false){
-		img.src = 'images/win.jpg';
+	if(randomNum === imgId && img.class != 'sad' && gameOver==false){ // check if we have a match, and the click is not from an already sad image, and that the game is still running
+		img.src = 'images/win.jpg';									  // alter image source
 		winOrLose.innerHTML = 'win.';
 		finalScore = ++attempts;
-		attemptCountDisplay.innerHTML = 'Final Score: ' + finalScore +' attempts';
+		attemptCountDisplay.innerHTML = 'Final Score: ' + finalScore +' attempts'; // display the final score
 		gameOver = true;
-	} else if(img.class != "sad" && gameOver==false) {
+		clearTimeout(stopTime);
+	} else if(img.class != "sad" && gameOver==false) { //if the image is not already sad and the game is still running
 		if(attempts < 4) {
 			attempts++;
 			attemptCountDisplay.innerHTML = attempts + "/4 attempts. " + randomText;
@@ -68,7 +76,8 @@ function winLose(img, imgId){
 		if(attempts == 4) {
 			attemptCountDisplay.innerHTML = "You've used up all of your attempts. Click on any face to play again.";
 			winOrLose.innerHTML = '<span style="color:red;">lose.</span>'; //you lose prompt
-			gameOver = true;	
+			gameOver = true;
+			clearTimeout(stopTime);	
 		}
 	}
 }
